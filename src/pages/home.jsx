1f08841743fdce76d5b90ea3c0497783ce0e046a
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-03 23:06:10 -0500

    carousel layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import styles from './home.module.css'
 import Carousel from "../components/Carousel/Carousel";
 import Card from "../components/Card/Card";
+import promo_0001 from "../assets/photos/promo_0001.png"
 
 export default function Home() {
+
+  const imageArray = [
+    promo_0001
+  ]
+
   return (
     <div className={styles.container}>
-      
+      <Carousel imgArray={imageArray}/>
     </div>
   );
 }
