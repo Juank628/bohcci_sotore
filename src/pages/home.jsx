35ae301c0e23098d2c96057ea4fa61db403ea968
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-04 23:41:37 -0500

    Cards and gallery mobile first

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from './home.module.css'
 import Carousel from "../components/Carousel/Carousel";
-import Card from "../components/Card/Card";
-import promo_0001 from "../assets/photos/promo_0001.png"
+import Gallery from "../components/Gallery/Gallery";
+import promo_0001 from "../assets/photos/art/promo_0001.png"
 
 export default function Home() {
 
@@ -11,8 +11,9 @@ export default function Home() {
   ]
 
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
       <Carousel imgArray={imageArray}/>
-    </div>
+      <Gallery/>
+    </section>
   );
 }
