65966f94c644b30f47503debff701abe48007517
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-12 23:31:01 -0500

    Gallery responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from './home.module.css'
 import Carousel from "../components/Carousel/Carousel";
 import Gallery from "../components/Gallery/Gallery";
-import Footer from "../components/Footer/Footer";
 
 export default function Home() {
 
@@ -10,7 +9,6 @@ export default function Home() {
     <section className={styles.container}>
       <Carousel/>
       <Gallery/>
-      <Footer />
     </section>
   );
 }
--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./root.scss"
 import {Outlet} from 'react-router-dom'
 import Navbar from '../components/Navbar/Navbar'
+import Footer from '../components/Footer/Footer'
 
 export default function Root() {
   return (
     <div>
         <Navbar />
         <Outlet />
+        <Footer />
     </div>
   )
 }
