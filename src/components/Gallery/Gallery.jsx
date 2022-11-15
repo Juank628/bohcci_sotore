75cd5a0b86c65ed25fdcb06c839f53858b0e9a7f
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-14 21:56:52 -0500

    Gallery photo template update

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import styles from "./Gallery.module.scss"
-import Card from "../Card/Card"
-import cover_007 from "../../assets/photos/covers/cover_007.jpeg"
+import React from "react";
+import styles from "./Gallery.module.scss";
+import Card from "../Card/Card";
+import cover_007 from "../../assets/photos/covers/cover_007.jpeg";
+import template_2_3 from "../../assets/photos/covers/template_2_3.jpg";
 
 export default function Gallery() {
-
-    const cards_info = [
-        {
-            img: cover_007
-        },
-    ]
+  const cards_info = [
+    {
+      img: cover_007,
+    },
+    {
+      img: template_2_3,
+    },
+  ];
 
   return (
     <section className={styles.container}>
-        <Card card_info={cards_info[0]}/>
-        <Card card_info={cards_info[0]}/>
-        <Card card_info={cards_info[0]}/>
-        <Card card_info={cards_info[0]}/>
-        <Card card_info={cards_info[0]}/>
-        <Card card_info={cards_info[0]}/>
+      <Card card_info={cards_info[1]} />
+      <Card card_info={cards_info[1]} />
+      <Card card_info={cards_info[1]} />
+      <Card card_info={cards_info[1]} />
+      <Card card_info={cards_info[1]} />
+      <Card card_info={cards_info[1]} />
     </section>
-  )
+  );
 }
