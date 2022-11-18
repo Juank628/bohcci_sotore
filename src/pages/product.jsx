696afb81eec2ff783c9d3ad356bf26603396abf8
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-17 22:10:37 -0500

    Product detail desktop HTML and CSS

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,26 +1,36 @@
-import React from "react";
-import styles from "./product.module.scss";
-import template_2_3 from "../assets/photos/covers/template_2_3.jpg";
+import React from 'react';
+import styles from './product.module.scss';
+import template_2_3 from '../assets/photos/covers/template_2_3.jpg';
 
 export default function Product() {
   return (
     <section className={styles.container}>
-      <img className={styles.main_photo} src={template_2_3} alt="" />
-      <h2 className={styles.title}>Salida Vichayito - Verde</h2>
-      <p className={styles.price}>S/120</p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quaerat
-        ratione cupiditate corporis aliquam! Consectetur veritatis quod
-        recusandae. Rerum, fuga officiis consequuntur voluptatem adipisci et
-        necessitatibus iusto quaerat consectetur deleniti.
-      </p>
-      <p className={styles.subtitle}>Talla</p>
-      <ul className={styles.sizes_list}>
-        <li className={styles.size}>S</li>
-        <li className={styles.size}>M</li>
-        <li className={styles.size}>L</li>
-      </ul>
-      <button className={styles.btn_wide}>Agregar al carrito</button>
+      <div className={styles.photos}>
+        <img className={styles.main_photo} src={template_2_3} alt='' />
+      </div>
+      <div className={styles.info}>
+        <h2 className={styles.title}>Salida Vichayito - Verde</h2>
+        <p className={styles.price}>S/120</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea quaerat
+          ratione cupiditate corporis aliquam! Consectetur veritatis quod
+          recusandae. Rerum, fuga officiis consequuntur voluptatem adipisci et
+          necessitatibus iusto quaerat consectetur deleniti.
+        </p>
+        <p className={styles.subtitle}>Talla</p>
+        <ul className={styles.sizes_list}>
+          <li className={styles.size}>S</li>
+          <li className={styles.size}>M</li>
+          <li className={styles.size}>L</li>
+        </ul>
+        <p className={styles.subtitle}>Cantidad</p>
+        <div className={styles.quantity}>
+          <span>&lt;</span>
+          <input type="text" />
+          <span>&gt;</span>
+        </div>
+        <button className={styles.add_cart_btn}>Agregar al carrito</button>
+      </div>
     </section>
   );
 }
