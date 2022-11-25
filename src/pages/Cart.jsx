bc0f7016ee18d85a1b4820f0f07ac962ecdbf2bc
Juank628 <jsanchez_zunino@yahoo.com>
2022-11-24 19:44:47 -0500

    cart desktop view

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import styles from './Cart.module.scss'
-import CartItem from '../components/CartItem/CartItem'
+import React, {useState} from 'react';
+import styles from './Cart.module.scss';
+import CartItem from '../components/CartItem/CartItem';
 
 export default function Cart() {
+  const [dinamicMargin, setDinamicMargin] = useState({marginTop: 0})
+  
+  document.addEventListener('scroll', () => {
+    const scroll = document.documentElement.scrollTop
+    const viewPortX = window.innerWidth
+
+    if(viewPortX >= 992) {
+      setDinamicMargin({
+        marginTop: scroll
+      })
+    }
+  })
+
+  const buy = () => {
+    console.log('in development')
+  }
+
   return (
     <section className={styles.container}>
+      <div className={styles.cards_container}>
         <CartItem />
         <CartItem />
         <CartItem />
-        <section className={styles.total}>
-          <p className={styles.text}>Total</p>
-          <p className={styles.price}>S/250.00</p>
-          <button className={styles.buy_button}>Continuar compra</button>
-        </section>
+      </div>
+
+      <section className={styles.total_container} style={dinamicMargin}>
+        <p className={styles.text}>Total</p>
+        <p className={styles.price}>S/250.00</p>
+        <button className={styles.buy_button} onClick={buy}>Continuar compra</button>
+      </section>
     </section>
-  )
+  );
 }
