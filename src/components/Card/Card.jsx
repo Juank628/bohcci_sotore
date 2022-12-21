f2ac55e07c825b07b7c36589aff6abf2ecdc904b
Juank628 <jsanchez_zunino@yahoo.com>
2022-12-20 21:51:04 -0500

    product photos moved to public folder

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -8,11 +8,11 @@ export default function Card(props) {
   return (
     <section className={styles.container}>
         <div className={styles.img_area}>
-          <img src={null} alt="" />
+          <img src={`./photos/products/${data.id}.jpg`} alt="" />
         </div>
         <div className={styles.info}>
-            <p className={styles.title}>${data.name}</p>
-            <p className={styles.price}>${data.price}</p>
+            <p className={styles.title}> {data.name}</p>
+            <p className={styles.price}> S/{data.price}</p>
         </div>
     </section>
   )
