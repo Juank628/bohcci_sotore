dcf990d595866e4c7bdc15564cf90fd1987364da
Juank628 <jsanchez_zunino@yahoo.com>
2022-12-26 23:35:48 -0500

    empty cart message

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { clearCart } from '../features/cart/cartSlice';
 import styles from './cart.module.scss';
 import CartItem from '../components/CartItem/CartItem';
+import emptyCartIcon from '../assets/icons/empty_cart_icon.png'
 
 export default function Cart() {
   const dispatch = useDispatch();
@@ -26,19 +27,28 @@ export default function Cart() {
 
   return (
     <section className={styles.container}>
-      <div className={styles.cards_container}>
-        {cartItems.map((item, index) => (
-          <CartItem key={index} data={item} />
-        ))}
-      </div>
+      {cartItems.length > 0 ? (
+        <Fragment>
+          <div className={styles.cards_container}>
+            {cartItems.map((item, index) => (
+              <CartItem key={index} data={item} />
+            ))}
+          </div>
 
-      <section className={styles.total_container} style={dinamicMargin}>
-        <p className={styles.text}>Total</p>
-        <p className={styles.price}>S/250.00</p>
-        <button type='button' className={styles.buy_button} onClick={buy}>
-          Continuar compra
-        </button>
-      </section>
+          <section className={styles.total_container} style={dinamicMargin}>
+            <p className={styles.text}>Total</p>
+            <p className={styles.price}>S/250.00</p>
+            <button type='button' className={styles.buy_button} onClick={buy}>
+              Continuar compra
+            </button>
+          </section>
+        </Fragment>
+      ) : (
+          <div className={styles.empty_cart_container}>
+            <p className={styles.empty_cart_text}>No has agregado ningún producto al carrito...</p>
+            <img className={styles.empty_cart_img} src={emptyCartIcon} alt="" />
+          </div>
+      )}
     </section>
   );
 }
