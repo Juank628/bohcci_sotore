604190d8f1eb4ad2c77b7d399b372baff82e15bf
Juank628 <jsanchez_zunino@yahoo.com>
2023-01-19 23:24:53 -0500

    replace createBrowserRouter by BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,43 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './assets/style/normalize.css';
 import Root from './pages/root';
-import Home from './pages/home';
-import Shop from './pages/shop';
-import Product from './pages/product';
-import Cart from './pages/cart';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    children: [
-      {
-        path: 'home',
-        element: <Home />,
-      },
-      {
-        path: 'shop',
-        element: <Shop />,
-      },
-      {
-        path: 'product',
-        element: <Product />,
-      },
-      {
-        path: 'cart',
-        element: <Cart />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
+  </Provider>
 );
