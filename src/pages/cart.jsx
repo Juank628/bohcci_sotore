f0df736b06635821f733e6fe7f2b924fa247738e
Juank628 <jsanchez_zunino@yahoo.com>
2023-02-17 15:13:55 -0500

    order data to query string for Whatsapp

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import styles from './cart.module.scss';
 import CartItem from '../components/CartItem/CartItem';
@@ -6,6 +6,8 @@ import emptyCartIcon from '../assets/icons/empty_cart_icon.png';
 
 export default function Cart() {
   const { cartItems } = useSelector((store) => store.cart);
+  const { list } = useSelector((store) => store.products);
+  const [whatsAppText, setWhatsAppText] = useState('');
   const [dinamicMargin, setDinamicMargin] = useState({ marginTop: 0 });
 
   document.addEventListener('scroll', () => {
@@ -18,6 +20,20 @@ export default function Cart() {
       });
     }
   });
+
+  const buildWhatsAppText = () => {
+    let productName = '';
+    let text = 'Hola 👋 estoy interesad@ en los siguientes productos: %0D%0A';
+    cartItems.forEach((item, index) => {
+      productName = list.find((product) => product.id === item.id).name;
+      text += `*item: ${index + 1}* %0D%0Aproducto: ${productName} %0D%0Atalla: ${item.size} %0D%0Acantidad: ${item.quantity} %0D%0A`;
+    });
+    setWhatsAppText(text);
+  };
+
+  useEffect(() => {
+    buildWhatsAppText();
+  }, []);
 
   return (
     <section className={styles.container}>
@@ -32,7 +48,7 @@ export default function Cart() {
           <section className={styles.total_container} style={dinamicMargin}>
             <p className={styles.text}>Total</p>
             <p className={styles.price}>S/250.00</p>
-            <a href="https://api.whatsapp.com/send?phone=956382973&text=%20test" target="_blank" rel="noreferrer" className={styles.buy_button}>
+            <a href={`https://api.whatsapp.com/send?phone=956382973&text=${whatsAppText}`} target="_blank" rel="noreferrer" className={styles.buy_button}>
               Continuar compra
             </a>
           </section>
